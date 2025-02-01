774952731886ca0cca7baa2457f8a7476b0fed11
David Tolnay <dtolnay@gmail.com>
2025-02-01 14:03:14 -0800

    Add Parse impls

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 use proc_macro2::TokenStream;
+use syn::parse::{Error, Parse, ParseStream, Result};
 use syn::punctuated::Punctuated;
 use syn::token::{Brace, Bracket, Paren};
-use syn::{LitStr, Token};
+use syn::{braced, bracketed, parenthesized, LitStr, Token};
 
 mod kw {
     syn::custom_keyword!(concat);
@@ -62,4 +63,103 @@ enum MacroDelimiter {
     Paren(Paren),
     Brace(Brace),
     Bracket(Bracket),
+}
+
+impl Parse for Expr {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let lookahead = input.lookahead1();
+        if lookahead.peek(LitStr) {
+            input.parse().map(Expr::Lit)
+        } else if lookahead.peek(kw::concat) {
+            input.parse().map(Expr::Concat)
+        } else if lookahead.peek(kw::env) {
+            input.parse().map(Expr::Env)
+        } else if lookahead.peek(kw::include) {
+            input.parse().map(Expr::Include)
+        } else if lookahead.peek(kw::include_str) {
+            input.parse().map(Expr::IncludeStr)
+        } else if lookahead.peek(kw::stringify) {
+            input.parse().map(Expr::Stringify)
+        } else {
+            Err(lookahead.error())
+        }
+    }
+}
+
+macro_rules! macro_delimiter {
+    ($var:ident in $input:ident) => {{
+        let lookahead = $input.lookahead1();
+        if $input.peek(Paren) {
+            MacroDelimiter::Paren(parenthesized!($var in $input))
+        } else if $input.peek(Brace) {
+            MacroDelimiter::Brace(braced!($var in $input))
+        } else if $input.peek(Bracket) {
+            MacroDelimiter::Bracket(bracketed!($var in $input))
+        } else {
+            return Err(lookahead.error());
+        }
+    }};
+}
+
+impl Parse for Concat {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let content;
+        Ok(Concat {
+            name: input.parse()?,
+            bang_token: input.parse()?,
+            delimiter: macro_delimiter!(content in input),
+            args: content.call(Punctuated::parse_terminated)?,
+        })
+    }
+}
+
+impl Parse for Env {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let content;
+        Ok(Env {
+            name: input.parse()?,
+            bang_token: input.parse()?,
+            delimiter: macro_delimiter!(content in input),
+            arg: content.parse()?,
+            trailing_comma: content.parse()?,
+        })
+    }
+}
+
+impl Parse for Include {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let content;
+        Ok(Include {
+            name: input.parse()?,
+            bang_token: input.parse()?,
+            delimiter: macro_delimiter!(content in input),
+            arg: content.parse()?,
+            trailing_comma: content.parse()?,
+        })
+    }
+}
+
+impl Parse for IncludeStr {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let content;
+        Ok(IncludeStr {
+            name: input.parse()?,
+            bang_token: input.parse()?,
+            delimiter: macro_delimiter!(content in input),
+            arg: content.parse()?,
+            trailing_comma: content.parse()?,
+        })
+    }
+}
+
+impl Parse for Stringify {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let content;
+        Ok(Stringify {
+            name: input.parse()?,
+            bang_token: input.parse()?,
+            delimiter: macro_delimiter!(content in input),
+            tokens: content.parse()?,
+        })
+    }
 }
