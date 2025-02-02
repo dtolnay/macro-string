dcd0017a4821d8fe3217371c66ac8a89d4d2cc2a
David Tolnay <dtolnay@gmail.com>
2025-02-01 18:29:54 -0800

    Resolve doctest compilation

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,10 @@
 //! For example, to implement a macro such as the following:
 //!
 //! ```
+//! # macro_rules! include_json {
+//! #     ($path:expr) => { $path };
+//! # }
+//! #
 //! // Parses JSON at compile time and expands to a serde_json::Value.
 //! let j = include_json!(concat!(env!("CARGO_MANIFEST_DIR"), "/manifest.json"));
 //! ```
@@ -13,12 +17,17 @@
 //! evaluate the two macro calls within its input tokens.
 //!
 //! ```
+//! # extern crate proc_macro;
+//! #
 //! use macro_string::MacroString;
 //! use proc_macro::TokenStream;
+//! use proc_macro2::Span;
 //! use std::fs;
 //! use syn::parse_macro_input;
 //!
+//! # const _: &str = stringify! {
 //! #[proc_macro]
+//! # };
 //! pub fn include_json(input: TokenStream) -> TokenStream {
 //!     let MacroString(path) = parse_macro_input!(input);
 //!
@@ -37,6 +46,7 @@
 //!     };
 //!
 //!     /*TODO: print serde_json::Value to TokenStream*/
+//!     # unimplemented!()
 //! }
 //! ```
 
