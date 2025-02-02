d58584ddb06b3a4597a4efddf0a2c207b0e154fb
David Tolnay <dtolnay@gmail.com>
2025-02-02 14:26:29 -0800

    Release 0.1.1

--- src/lib.rs
+++ src/lib.rs
@@ -58,7 +58,7 @@
 //! }
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/macro-string/0.1.0")]
+#![doc(html_root_url = "https://docs.rs/macro-string/0.1.1")]
 
 use proc_macro2::TokenStream;
 use quote::{quote, ToTokens};
